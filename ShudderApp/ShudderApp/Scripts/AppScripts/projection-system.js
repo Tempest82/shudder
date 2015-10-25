79154283e4cbcff287693e51a1980533d2a836a0
Tempest_82 <webdevx@gmail.com>
2015-10-25 10:40:15 -0400

    Trying to get sim working , save effort

--- ShudderApp/ShudderApp/Scripts/AppScripts/projection-system.js
+++ ShudderApp/ShudderApp/Scripts/AppScripts/projection-system.js
@@ -1,9 +1,9 @@
 ﻿(function (hud, $, undefined) {
 
     hud.degreesVisible = 114;
-    hud.halfDegreesVisible = hud.degreesVisible/2;
+    hud.halfDegreesVisible = hud.degreesVisible / 2;
     hud.determineVisible = function (targetCompassAngle, targetGroundAngle) {
-        if ( 
+        if (
             (
                 (targetCompassAngle >= (hud.compassAngle - hud.halfDegreesVisible) && targetCompassAngle <= (hud.compassAngle + hud.halfDegreesVisible))
                   || (targetCompassAngle >= (hud.compassAngle + 360 - hud.halfDegreesVisible) && targetCompassAngle <= (hud.compassAngle + 360 + hud.halfDegreesVisible))
@@ -33,13 +33,11 @@
         newGroundAngle = hud.groundAngle;
         newTargetCompassAngle = targetCompassAngle;
         newTargetGroundAngle = targetGroundAngle;
-        if (Math.abs(newTargetCompassAngle - newCompassAngle) > hud.halfDegreesVisible)
-        {
+        if (Math.abs(newTargetCompassAngle - newCompassAngle) > hud.halfDegreesVisible) {
             if (newCompassAngle > (360 - hud.halfDegreesVisible)) {
                 newTargetCompassAngle += 360;
             }
-            else
-            {
+            else {
                 newCompassAngle += 360;
             }
         }
@@ -54,17 +52,42 @@
         var degreeW = w / hud.degreesVisible;
         var degreeH = h / hud.degreesVisible;
         var leftmostAngle = newCompassAngle - hud.halfDegreesVisible;
-        if (leftmostAngle < 0 )
-            {leftmostAngle += 360;}
+        if (leftmostAngle < 0)
+        { leftmostAngle += 360; }
         var topMostAngle = newGroundAngle + hud.halfDegreesVisible;
-        var topPosition = (topMostAngle - newTargetGroundAngle ) * degreeH;
+        var topPosition = (topMostAngle - newTargetGroundAngle) * degreeH;
 
         if (newTargetCompassAngle < leftmostAngle)
-        {newTargetCompassAngle += 360;}
+        { newTargetCompassAngle += 360; }
         var leftPosition = (newTargetCompassAngle - leftmostAngle) * degreeW;
         var result = { left: leftPosition, top: topPosition }
         return (result);
     };
-
+    hud.drawTargets = function () {
+        //read array
+        console.log(window.targets.length);
+        for (var i = 0; i < window.targets.length; i++) {
+            //delete all targets indiscriminatly, update this to avoid some of the lag
+            $("img[id^='target']").remove();
+            var theTarget = window.targets[i];
+            if (hud.determineVisible(theTarget.position.bearing, theTarget.position.elevationAngle)) {
+                var imgsize = hud.determineSize(theTarget.position.distance);
+                var position = hud.determineLocation(theTarget.position.bearing, theTarget.position.elevationAngle);
+                var targetName = "target" + theTarget.id;
+                if ($("#" + targetName).length == 0) {
+                    //it doesn't exist
+                    $('#sphere-box2').append('<img src="/Content/Images/Source/r2d2-512.png" style="position:absolute;" id="' + targetName + '" />');
+                }
+                document.getElementById(targetName).style.height = imgsize + '%';
+                document.getElementById(targetName).style.width = imgsize + '%';
+                document.getElementById(targetName).style.top = (position.top - (document.getElementById(targetName).clientHeight / 2)) + 'px';
+                document.getElementById(targetName).style.left = (position.left - (document.getElementById(targetName).clientWidth / 2)) + 'px';
+                console.log('Showing target ' + theTarget.id);
+            }
+            else {
+                console.log('not showing target ' + theTarget.id);
+            }
+        }
+    }
 
 }(window.hud = window.hud || {}, jQuery));
