f7c503940fef02d8ea13cf16125e97a64ad69126
Tempest_82 <webdevx@gmail.com>
2015-10-24 21:25:29 -0400

    updated hudtest for compass

--- ShudderApp/ShudderApp/Scripts/AppScripts/hud-system.js
+++ ShudderApp/ShudderApp/Scripts/AppScripts/hud-system.js
@@ -1,1 +1,34 @@
 ﻿
+
+(function (hud, $, undefined) {
+
+
+    hud.test =  // Obtain a new *world-oriented* Full Tilt JS DeviceOrientation Promise
+  var fulltiltPromise = FULLTILT.getDeviceOrientation({ 'type': 'world' });
+
+    // Wait for Promise result
+    fulltiltPromise.then(function(deviceOrientation) { // Device Orientation Events are supported
+
+        // Register a callback to run every time a new 
+        // deviceorientation event is fired by the browser.
+        deviceOrientation.listen(function() {
+
+            // Get the current *screen-adjusted* device orientation angles
+            var currentOrientation = deviceOrientation.getScreenAdjustedEuler();
+
+            // Calculate the current compass heading that the user is 'looking at' (in degrees)
+            var compassHeading = 360 - currentOrientation.alpha;
+
+            // Do something with `compassHeading` here...
+
+        });
+
+    }).catch(function(errorMessage) { // Device Orientation Events are not supported
+
+        console.log(errorMessage);
+
+        // Implement some fallback controls here...
+
+    });
+
+}(window.hud = window.hud || {}, jQuery));
