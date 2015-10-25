06cb71e5b25cd01e2f4d57deb59671818e1bd0eb
Tempest_82 <webdevx@gmail.com>
2015-10-25 14:01:39 -0400

    updated systems

--- ShudderApp/ShudderApp/Scripts/AppScripts/projection-system.js
+++ ShudderApp/ShudderApp/Scripts/AppScripts/projection-system.js
@@ -77,7 +77,12 @@
                 var targetName = "target" + theTarget.id;
                 if ($("#" + targetName).length == 0) {
                     //it doesn't exist
-                    $('#sphere-box2').append('<img src="/Content/Images/Source/r2d2-512.png" style="position:absolute;opacity: 0.4;" id="' + targetName + '" />');
+                    if (theTarget.unitType == "Drone") {
+                        $('#sphere-box2').append('<img src="/Content/Images/Source/uav-512.png" style="position:absolute;opacity: 0.4;" id="' + targetName + '" />');
+                    }
+                    else {
+                        $('#sphere-box2').append('<img src="/Content/Images/Source/r2d2-512.png" style="position:absolute;opacity: 0.4;" id="' + targetName + '" />');
+                    }
                 }
                 document.getElementById(targetName).style.height = imgsize + '%';
                 document.getElementById(targetName).style.width = imgsize + '%';
