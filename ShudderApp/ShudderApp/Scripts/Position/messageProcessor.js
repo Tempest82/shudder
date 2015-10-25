a2ddf9880b06b0f5ea8790064a70e6519ef83e3f
Nathan McEwen <nod757@hotmail.com>
2015-10-24 23:38:09 -0400

    Added wall intersection logic.

--- ShudderApp/ShudderApp/Scripts/Position/messageProcessor.js
+++ ShudderApp/ShudderApp/Scripts/Position/messageProcessor.js
@@ -20,7 +20,9 @@ function ProcessIncomingMessage(positionMessage) {
             latitude: position.latitude,
             longitude: position.longitude,
             elevation: position.elevation,
-            elevationAngle: positionData.VerticalBearing
+            elevationAngle: positionData.VerticalBearing,
+            obstructions: positionData.Obstructions,
+            totalObstructions: positionData.TotalObstructions
         },
         unitType: positionMessage.unit,
         timestamp: Date.now()
@@ -29,6 +31,72 @@ function ProcessIncomingMessage(positionMessage) {
 
 function TranslatePosition(lat, long, elevation, direction) {
     GetCurrentLocation();
+
+    var sofwerx = {
+        "type": "LineString",
+        "coordinates": [
+          [-82.4636437,
+            27.9607638
+          ],
+          [-82.4643819,
+            27.9607669
+          ],
+          [-82.4643778,
+            27.9615415
+          ],
+          [-82.4636332,
+            27.9615384
+          ],
+          [-82.4636344,
+            27.961315
+          ],
+          [-82.4637009,
+            27.9613152
+          ],
+          [-82.4637018,
+            27.9611526
+          ],
+          [-82.4636588,
+            27.9611524
+          ],
+          [-82.4636592,
+            27.9610946
+          ],
+          [-82.4636995,
+            27.9610948
+          ],
+          [-82.4637034535408,
+            27.961047192579947
+          ],
+          [-82.4637756,
+            27.9610518
+          ],
+          [-82.4637759,
+            27.9609919
+          ],
+          [-82.4636425,
+            27.9609914
+          ],
+          [-82.4636437,
+            27.9607638
+          ]
+        ]
+    };
+
+    var position1 = {
+        "coordinates": [
+          [
+            myPosition.longitude,
+            myPosition.latitude
+          ],
+          [
+            long,
+            lat
+          ]
+        ]
+    };
+
+    var intersectionPoints = lineStringsIntersect(sofwerx, position1);
 
     var pi = Math.PI;
     lat *= pi / 180;
@@ -65,6 +133,8 @@ function TranslatePosition(lat, long, elevation, direction) {
     positionalData.StraightDistance = straightLineDistance;
     positionalData.Bearing = bearing;
     positionalData.VerticalBearing = verticalAngle;
+    positionalData.Obstructions = intersectionPoints;
+    positionalData.TotalObstructions = intersectionPoints.length || 0;
 
     return positionalData;
 }
@@ -73,4 +143,43 @@ function GetCurrentLocation() {
     myPosition.latitude = window.hud.latitude;
     myPosition.longitude = window.hud.longitude;
     myPosition.elevation = window.hud.elevation;
+}
+
+function lineStringsIntersect(l1, l2) {
+    var intersects = [];
+    for (var i = 0; i <= l1.coordinates.length - 2; ++i) {
+        for (var j = 0; j <= l2.coordinates.length - 2; ++j) {
+            var a1 = {
+                x: l1.coordinates[i][1],
+                y: l1.coordinates[i][0]
+            },
+              a2 = {
+                  x: l1.coordinates[i + 1][1],
+                  y: l1.coordinates[i + 1][0]
+              },
+              b1 = {
+                  x: l2.coordinates[j][1],
+                  y: l2.coordinates[j][0]
+              },
+              b2 = {
+                  x: l2.coordinates[j + 1][1],
+                  y: l2.coordinates[j + 1][0]
+              },
+              ua_t = (b2.x - b1.x) * (a1.y - b1.y) - (b2.y - b1.y) * (a1.x - b1.x),
+              ub_t = (a2.x - a1.x) * (a1.y - b1.y) - (a2.y - a1.y) * (a1.x - b1.x),
+              u_b = (b2.y - b1.y) * (a2.x - a1.x) - (b2.x - b1.x) * (a2.y - a1.y);
+            if (u_b != 0) {
+                var ua = ua_t / u_b,
+                  ub = ub_t / u_b;
+                if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
+                    intersects.push({
+                        'type': 'Point',
+                        'coordinates': [a1.x + ua * (a2.x - a1.x), a1.y + ua * (a2.y - a1.y)]
+                    });
+                }
+            }
+        }
+    }
+
+    return intersects;
 }
